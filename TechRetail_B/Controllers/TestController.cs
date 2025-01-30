5e938cee16abec2bd012addd650264b284fb2368
Elia237 <eliastrazza.es@gmail.com>
2025-01-30 17:15:48 +0100

    fix data
    FUNZIONA LA DATA

--- TechRetail_B/Controllers/TestController.cs
+++ TechRetail_B/Controllers/TestController.cs
@@ -7,27 +7,27 @@ namespace TechRetail_B.Controllers
     {
         public IActionResult Index()
         {
-            //int id = 1;
-            //int id2 = 2;
-            //Entity f = DAOFiliali.GetInstance().FindRecord(id);
-            //Entity f2 = DAOProdotti.GetInstance().FindRecord(id);
-            //Entity f3 = DAOUtenti.GetInstance().FindRecord(id);
-            //Entity f4 = DAOFiliali.GetInstance().FindRecord(id2);
-            //Ordine e = new Ordine()
-            //{
-            //    Data = new DateTime(30 / 01 / 2025),
-            //    Quantita = 10,
-            //    _Utente = (Utente)f3,
-            //    _Prodotto = (Prodotto)f2,
-            //    _FilialePartenza = (Filiale)f,
-            //    _FilialeArrivo = (Filiale)f4,
-            //    IndirizzoConsegna = "prova via casa di matteo",
-            //    InLoco = false,
-            //    Restock = true,
+            int id = 1;
+            int id2 = 2;
+            Entity f = DAOFiliali.GetInstance().FindRecord(id);
+            Entity f2 = DAOProdotti.GetInstance().FindRecord(id);
+            Entity f3 = DAOUtenti.GetInstance().FindRecord(id);
+            Entity f4 = DAOFiliali.GetInstance().FindRecord(id2);
+            Ordine e = new Ordine()
+            {
+                Data = new DateTime(day:25,month:1,year:2025),
+                Quantita = 10,
+                _Utente = (Utente)f3,
+                _Prodotto = (Prodotto)f2,
+                _FilialePartenza = (Filiale)f,
+                _FilialeArrivo = (Filiale)f4,
+                IndirizzoConsegna = "prova via casa di matteo",
+                InLoco = false,
+                Restock = true,
 
 
-            //};
-            //DAOOrdini.GetInstance().CreateRecord(e);
+            };
+            DAOOrdini.GetInstance().CreateRecord(e);
 
             DAOOrdini.GetInstance().DeleteRecord(15);
             
