493f448ff4642b89a7faaae2963f2b093e718181
Besky037 <Leo.Betti05@gmail.com>
2025-02-04 10:04:24 +0100

    Test Leo 1
    movimento cartelle con front end per vedere se va

---
+++ TECHRETAIL/Test3/schedatecnica/schedatecnica/index.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    var navbar = document.getElementById("Navbar");
+    var navIcon = document.getElementById("navIcon");
+    var dashboard = document.querySelector(".Dashboard");
+    var infoHeader = document.querySelector(".infoHeader");
+
+    navbar.classList.add("open");
+    navIcon.textContent = "✖";
+    dashboard.style.marginRight = "250px";
+    infoHeader.style.marginRight = "250px";
+    };
+
+    // Funzione per aprire/chiudere la navbar
+    function toggleNavbar() {
+    var navbar = document.getElementById("Navbar");
+    var navIcon = document.getElementById("navIcon");
+    var dashboard = document.querySelector(".product-details");
+    var infoHeader = document.querySelector(".infoHeader");
+
+    navbar.classList.toggle("open");
+
+    if (navbar.classList.contains("open")) {
+        navIcon.textContent = "✖";
+        dashboard.style.marginRight = "250px"; // Sposta i contenuti quando la navbar è aperta
+        infoHeader.style.marginRight = "250px";
+    } else {
+        navIcon.textContent = "☰";
+        dashboard.style.marginRight = "0"; // Riporta i contenuti alla posizione iniziale
+        infoHeader.style.marginRight = "0";
+    }
+
+
+    }
